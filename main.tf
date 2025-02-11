657d30351a9dc81007c8ebc8fd2c0829103cbb0f
Vanessa <71130347+typewriterbot@users.noreply.github.com>
2025-02-11 00:17:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "swen614-terraform-bucket-test2"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"  # Set AWS region to US East 1 (N. Virginia)
