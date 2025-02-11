dc7af10e3d00b5c660b91ab8ad42d17045fa3205
Vanessa <71130347+typewriterbot@users.noreply.github.com>
2025-02-11 12:06:34 -0500

    Add files via upload

--- modules/internet_gateway/main.tf
+++ modules/internet_gateway/main.tf
@@ -1,1 +1,22 @@
+resource "aws_internet_gateway" "wordpress_igw" {
+  vpc_id = var.vpc_id
+  tags = {
+    Name = "WordPress Internet Gateway"
+  }
+}
 
+resource "aws_route_table" "public_rt" {
+  vpc_id = var.vpc_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.wordpress_igw.id
+  }
+  tags = {
+    Name = "WordPress Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_rta" {
+  subnet_id      = var.public_subnet_id
+  route_table_id = aws_route_table.public_rt.id
+}
---
+++ modules/internet_gateway/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "The ID of the public subnet"
+  type        = string
+}
