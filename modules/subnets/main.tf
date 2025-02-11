da13e4c818bc469cd2ddc479895c7b04e22cbb1c
Vanessa <71130347+typewriterbot@users.noreply.github.com>
2025-02-11 12:07:32 -0500

    Add files via upload

--- modules/subnets/main.tf
+++ modules/subnets/main.tf
@@ -1,1 +1,26 @@
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zones[0]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "WordPress Public Subnet"
+  }
+}
 
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = var.vpc_id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.availability_zones[1]
+  tags = {
+    Name = "WordPress Private Subnet"
+  }
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
---
+++ modules/subnets/varaibles.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
