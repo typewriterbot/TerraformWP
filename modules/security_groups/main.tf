c79f130a3bc0b40d93aa0788dabf39344b3799f0
Vanessa <71130347+typewriterbot@users.noreply.github.com>
2025-02-11 12:07:15 -0500

    Add files via upload

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -1,1 +1,47 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "wordpress_ec2_sg"
+  description = "Security group for WordPress EC2 instance"
+  vpc_id      = var.vpc_id
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "wordpress_rds_sg"
+  description = "Security group for WordPress RDS instance"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
+}
+
+output "ec2_sg_id" {
+  value = aws_security_group.ec2_sg.id
+}
+
+output "rds_sg_id" {
+  value = aws_security_group.rds_sg.id
+}
