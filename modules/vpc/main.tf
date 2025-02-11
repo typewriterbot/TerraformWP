b436da7001d1267099117adfe11136052f784a53
Vanessa <71130347+typewriterbot@users.noreply.github.com>
2025-02-11 12:07:50 -0500

    Add files via upload

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,12 @@
+resource "aws_vpc" "wordpress_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = var.vpc_name
+  }
+}
 
+output "vpc_id" {
+  value = aws_vpc.wordpress_vpc.id
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name tag for the VPC"
+  type        = string
+}
